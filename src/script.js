e7710dce1b85a48482f1e0aaa33394bedad4a4c1
Emrah Isovic <emrahisovic20@gmail.com>
2024-09-08 16:08:07 +0200

    JS update - use of classes instead of object constructors and updated overall project  structure

--- src/script.js
+++ src/script.js
@@ -1,143 +1,189 @@
-// All Book Objects will be within myLibrary Array.
-const myLibrary = [];
+"use strict"
 
+class Library {
+    constructor() {
+        this.myLibrary = [];
+    }
 
-const addBookBtnEl = document.querySelector('.add-button');
-const libraryEl = document.querySelector('.library');
-const bookDetailsFormEl = document.querySelector('.book-details__form');
-const bookDetailsCloseBtnEl = document.querySelector('.modal__close-button');
-const bookDetailsSubmitButtonEl = document.querySelector('.modal__submit-button')
+    addBook = (obj) => {
+        this.myLibrary.push(obj);
+    }
 
-// A modal that displays details for books
-const modal = document.querySelector('[data-modal]');
+    removeBook = (index) => {
+        this.myLibrary.splice(index, 1, "removed"); // we set removed element to "removed" because when we remove element, index numbers are changing
+    }
 
-function showBookDetails() {
-    // Show modal
-    modal.showModal();
-}
-
-function closeBookDetails() {
-    // Close modal
-    modal.close();
-}
-
-/* Book Constructor */ 
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read
-}
-
-Book.prototype.remove = function(index) {
-    myLibrary.splice(index, 1, "removed"); // we set removed element to "removed" because when we remove element, index numbers are changing
-}
-
-Book.prototype.changeStatus = function(status) {
-    this.read = status;
-}
-
-// Functions
-
-function bookDetailsSubmitted(e) {
-    e.preventDefault();
-
-    let formTitleInputValue = e.target.querySelector('#title').value;
-    let formAuthorInputValue = e.target.querySelector('#author').value;
-    let formPagesInputValue = +e.target.querySelector('#pages').value;
-    let formReadInputValue = e.target.querySelector('#read').checked;
-    
-    addBookToLibrary(formTitleInputValue, formAuthorInputValue, formPagesInputValue, formReadInputValue);
-
-    // Reset Input Values
-
-    e.target.querySelector('#title').value = '';
-    e.target.querySelector('#author').value = '';
-    e.target.querySelector('#pages').value = '';
-    e.target.querySelector('#read').checked = false;
-
-    closeBookDetails();
-}
-
-// Every new book "default"
-
-let bookEls = libraryEl.querySelectorAll('.library__book');
-let newDefaultBook = bookEls[0].cloneNode(true);
-
-function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead) {
-    
-    // CamelCase bookAuthor
-    let newBookAuthor = bookAuthor.split(' ')
-                                  .map((item, index) => index > 0 ? item.split('').map((item, index) => index === 0 ? item.toUpperCase() : item).join('') : item)
-                                  .join('');
-
-    // Add Book Object into myLibrary array.
-
-    myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookRead));
-
-    // Add Book Element into LibraryEl
-
-    let newBook = newDefaultBook.cloneNode(true);
-    
-    // Change Book Info values
-    newBook.querySelector('.library__book-info-heading').textContent = `${bookTitle}`;
-    newBook.querySelector('.library__book-info-author').textContent = `@by${newBookAuthor}`;
-    newBook.querySelector('.library__book-info-pages').textContent = `Pages: ${bookPages}`;
-    newBook.querySelector('.library__book-read').checked = bookRead;
-
-    // Associate book's read checkbox with label 
-
-    newBook.querySelector('.library__book-read').setAttribute("id", `book-read-${myLibrary.length - 1}`);
-    newBook.querySelector('.library__book-read').previousElementSibling.setAttribute("for", `book-read-${myLibrary.length - 1}`);
-    
-    // Add Book index data attribute
-    newBook.removeAttribute(`data-book-0`);
-    newBook.setAttribute(`data-book-${myLibrary.length - 1}`, "");
-   
-    libraryEl.appendChild(newBook);
-}
-
-
-const pragmaticProgrammer = new Book("Pragmatic Programmer","Andy Hunt", 320, false);
-const networking = new Book("Networking","Jim Kurose", 852, false);
-const recipesForCleanCode = new Book("Recipes for clean code", "Maximiliano Conieri", 428, false);
-
-myLibrary.push(pragmaticProgrammer);
-myLibrary.push(networking);
-myLibrary.push(recipesForCleanCode);
-
-
-// Removing books and Changing Read Status
-
-// We use this function to detect object of book that will be removed and to remove that book from UI.
-function detectBookObject(e) {
-    if(e.target.className === "library__book-close-button" || e.target.className === "library__book-close-button-span" || e.target.className === "library__book-read") {
-        
-        let closestBookEl =  e.target.closest(".library__book");
-
-        let bookAttributeArray = closestBookEl.getAttributeNames()[1].split('');
-        let objectIndex = +(bookAttributeArray[bookAttributeArray.length - 1]);
-
-        if(e.target.className === "library__book-close-button" || e.target.className === "library__book-close-button-span") {
-            //Remove Book Object
-            myLibrary[objectIndex].remove(objectIndex);
-            //Remove Book Card
-            closestBookEl.remove();
-        } else if(e.target.className === "library__book-read") {
-            let changedStatus = e.target.checked;
-            myLibrary[objectIndex].changeStatus(changedStatus);
-        }   
+    getLibrary() {
+        return this.myLibrary;
     }
 }
 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-//////////////////////////////////////////////////////////////////////////
-// Event Listeners
-//////////////////////////////////////////////////////////////////////////
+    changeReadStatus(status) {
+        this.read = status;
+        console.log(this);
+    }
+}
+
+// Library Control
+
+const LibraryController = function() {
+    const library = new Library();
+    
+    const addBookToLibrary = (obj) => library.addBook(obj);
+
+    const removeBookFromLibrary = (index) => library.removeBook(index);
+
+    const getMyLibrary = () => library.getLibrary();
+
+    // Default Books
+
+    const pragmaticProgrammer = new Book("Pragmatic Programmer","Andy Hunt", 320, false);
+    const networking = new Book("Networking","Jim Kurose", 852, false);
+    const recipesForCleanCode = new Book("Recipes for clean code", "Maximiliano Conieri", 428, false);
+
+    // Add Books to library
+
+    addBookToLibrary(pragmaticProgrammer);
+    addBookToLibrary(networking);
+    addBookToLibrary(recipesForCleanCode);
+
+    return { addBookToLibrary, removeBookFromLibrary, getMyLibrary }
+}
+
+// Screen Control
+
+const ScreenFlowControl = (function() {
+    const libraryControl = LibraryController();
+    const libraryEl = document.querySelector(".library");
+
+    // Update the library UI when we add a new book
+    const updateLibrary = (bookTitle, bookAuthor, bookPages, bookRead) => {
+        const bookEls = document.querySelectorAll(".library__book");
+
+        const newDefaultBook = bookEls[0].cloneNode(true);
+
+        const newBook = newDefaultBook.cloneNode(true);
+
+        const libraryLength = bookEls.length;
+
+        // Change Book Info values
+
+        newBook.querySelector('.library__book-info-heading').textContent = `${bookTitle}`;
+        newBook.querySelector('.library__book-info-pages').textContent = `Pages: ${bookPages}`;
+        newBook.querySelector('.library__book-read').checked = bookRead;
+        
+        const newBookAuthor = bookAuthor.split(' ')
+                                        .map((item, index) => index > 0 ? item.split('').map((item, index) => index === 0 ? item.toUpperCase() : item).join('') : item)
+                                        .join('');
+        
+        newBook.querySelector('.library__book-info-author').textContent = `@by${newBookAuthor}`;
+        
+
+        // Associate book's read checkbox with label 
+
+        newBook.querySelector('.library__book-read').setAttribute("id", `book-read-${libraryLength}`);
+        newBook.querySelector('.library__book-read').previousElementSibling.setAttribute("for", `book-read-${libraryLength}`);
+    
+        // Add Book index data attribute
+        newBook.removeAttribute(`data-book-0`);
+        newBook.setAttribute(`data-book-${libraryLength}`, "");
+   
+        libraryEl.appendChild(newBook);
+    }
+
+    // Functionality to open and close Book Details Modal
+
+    // A modal that displays details for books
+    const modal = document.querySelector('[data-modal]');
+
+    const bookDetailsOpenBtnEl = document.querySelector('.add-button');
+    const bookDetailsCloseBtnEl = document.querySelector('.modal__close-button');
+
+    const bookDetailsOpenBtnClickEventHandler = (e) => {
+        // Show modal
+        modal.showModal();
+    }
+    
+    const bookDetailsCloseBtnClickEventHandler = (e) => {
+        // Close modal
+        modal.close();
+    }
+
+    bookDetailsOpenBtnEl.addEventListener("click", bookDetailsOpenBtnClickEventHandler);
+    bookDetailsCloseBtnEl.addEventListener("click", bookDetailsCloseBtnClickEventHandler);
+
+    // Submit Details Form  
+
+    const bookDetailsFormEl = document.querySelector('.book-details__form');
+
+    const bookDetailsSubmitEventHandler = (e) => {
+        e.preventDefault();
+
+        const formTitleInputValue = e.target.querySelector('#title').value;
+        const formAuthorInputValue = e.target.querySelector('#author').value;
+        const formPagesInputValue = +e.target.querySelector('#pages').value;
+        const formReadInputValue = e.target.querySelector('#read').checked;
+
+        // Create Book and put it into library
+
+        const newBook = new Book(formTitleInputValue, formAuthorInputValue, formPagesInputValue, formReadInputValue);
+        libraryControl.addBookToLibrary(newBook);
+    
+        // Reset Input Values
+    
+        e.target.querySelector('#title').value = '';
+        e.target.querySelector('#author').value = '';
+        e.target.querySelector('#pages').value = '';
+        e.target.querySelector('#read').checked = false;
+
+        // Add book visually
+        updateLibrary(formTitleInputValue, formAuthorInputValue, formPagesInputValue, formReadInputValue);
+    
+        modal.close();
+    }
+
+    bookDetailsFormEl.addEventListener("submit", bookDetailsSubmitEventHandler);
 
 
-addBookBtnEl.addEventListener("click", showBookDetails);
-bookDetailsCloseBtnEl.addEventListener("click", closeBookDetails);
-bookDetailsFormEl.addEventListener("submit", bookDetailsSubmitted);
-libraryEl.addEventListener('click', detectBookObject);
+    // Remove Book Visually
+
+    const removeBookBtnClickEventHandler = (e) => {
+        if(e.target.className === "library__book-close-button" || e.target.className === "library__book-close-button-span") {
+            const closestBookEl = e.target.closest(".library__book");
+    
+            const bookAttributeArray = closestBookEl.getAttributeNames()[1].split('');
+            const objectIndex = +(bookAttributeArray[bookAttributeArray.length - 1]);
+
+            libraryControl.removeBookFromLibrary(objectIndex);
+    
+            closestBookEl.remove(); 
+        }
+    }
+
+    libraryEl.addEventListener("click", removeBookBtnClickEventHandler);
+
+    // Change read status
+
+    const readStatusBtnClickEventHandler = (e) => {
+        if(e.target.className === "library__book-read") {
+            const closestBookEl = e.target.closest(".library__book");
+    
+            const bookAttributeArray = closestBookEl.getAttributeNames()[1].split('');
+            const objectIndex = +(bookAttributeArray[bookAttributeArray.length - 1]);
+    
+            let changedStatusValue = e.target.checked;
+            libraryControl.getMyLibrary()[objectIndex].changeReadStatus(changedStatusValue);
+            console.log(libraryControl.getMyLibrary());
+        }
+    }
+
+    libraryEl.addEventListener("click", readStatusBtnClickEventHandler);
+})()
